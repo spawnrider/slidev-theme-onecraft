8dd8d7c92484596defcb2d3706875f35f48d01d1
Yohann Ciurlik <yohann.ciurlik@capgemini.com>
2025-04-04 16:56:45 +0200

    fix: issue when logo URL is not defined

--- utils/layoutHelper.ts
+++ utils/layoutHelper.ts
@@ -4,16 +4,13 @@ import type { CSSProperties } from 'vue'
  * Resolve urls from frontmatter and append with the base url
  */
 export function resolveAssetUrl(url: string) {
-  if (url.startsWith('/'))
+  if (url?.startsWith('/'))
     return import.meta.env.BASE_URL + url.slice(1)
   return url
 }
 
 export function handleBackground(background?: string, dim = false): CSSProperties {
   const isColor = background && (background[0] === '#' || background.startsWith('rgb'))
-
-  console.log("background", background);
-  console.log("isColor", isColor);
 
   const style = {
     background: isColor
@@ -39,8 +36,6 @@ export function handleBackground(background?: string, dim = false): CSSPropertie
 
   if (!style.background)
     delete style.background
-
-  console.log("style", style);
 
   return style
 }
